fac402ac590404511dc2db1b034057b0a407ddda
everdev <andy@ever.io>
2015-01-24 12:21:34 -0800

    first mission

--- levels/1.go
+++ levels/1.go
@@ -1,6 +1,14 @@
-/*
-* Welcome Aboard
-*/
+/**********************************************
+ * Mission: All Aboard
+ ***********************************************
+ *
+ * Logging into the agency network, you're quickly into the operation manifest source code.
+ * It looks like a typical agency snafu.
+ * You're going to need full combat gear for this mission.
+ * Let's go ahead and fix that.
+ *
+ */
+
 package main
 
 import(
@@ -8,5 +16,32 @@ import(
 )
 
 func main() {
-  fmt.Println("Welcome aboard")
+  total_agents := 5
+  agents := make([]Agent,0)
+  agents = append(agents, Agent{name: "Jason Marshall", equipment: "none"})
+  agents = append(agents, Agent{name: "Stone Boswell", equipment: "full"})
+  agents = append(agents, Agent{name: "Jane Johnson", equipment: "full"})
+  agents = append(agents, Agent{name: "Max Carter", equipment: "full"})
+  agents = append(agents, Agent{name: "Kay White", equipment: "full"})
+
+
+
+  printGearTable(agents, total_agents)
+}
+
+func printGearTable(agents []Agent, max_agents int) {
+  if len(agents) > max_agents {
+    fmt.Println("Error: too many soldiers")
+    return
+  }
+  fmt.Println("Operation Go: Agent Manifest")
+  fmt.Println("----------------------------")
+  for num, agent := range agents {
+    fmt.Println(num+1, agent.name, "| Gear:", agent.equipment)
+  }
+}
+
+type Agent struct {
+  name string
+  equipment string
 }
